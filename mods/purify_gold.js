3b5054dfca2403b8d7fcc91d182ff6c9237f0ee7
Ben-L-1 <110688017+Ben-L-1@users.noreply.github.com>
2023-03-30 10:10:38 -0400

    Create purify_gold.js

---
+++ mods/purify_gold.js
@@ -0,0 +1,16 @@
+elements.humbug = {
+    color: "#c9c5b1",
+    behavior: behaviors.LIQUID,
+    reactions: {
+        
+    },
+    viscosity: 0.56,
+    //tempHigh: 64.7,
+    burn: 100,
+    burnTime: 2,
+    fireColor: "#b2c5d1",
+    category: "mommy",
+    state: "liquid",
+    density: 2,
+    stain: -0.25,
+}
